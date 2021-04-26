c452787a110899874976892cc8fedd5ae166023b
邓豪 <denghao@equals.local>
2021-04-27 01:19:00 +0800

    alter

--- String/14.最长公共前缀  .py
+++ String/14.最长公共前缀  .py
@@ -1,5 +1,22 @@
 # Write a function to find the longest common prefix string amongst an array of strings.
 from numpy.core.tests.test_mem_overlap import xrange
+class Solution:
+    def longestCommonPrefix(self, strs):
+        def lcp(start, end):
+            if start == end:
+                return strs[start]
+
+            mid = (start + end) // 2
+            lcpLeft, lcpRight = lcp(start, mid), lcp(mid + 1, end)
+            minLength = min(len(lcpLeft), len(lcpRight))
+            for i in range(minLength):
+                if lcpLeft[i] != lcpRight[i]:
+                    return lcpLeft[:i]
+
+            return lcpLeft[:minLength]
+
+        return "" if not strs else lcp(0, len(strs) - 1)
+
 
 class Solution(object):
     def longestCommonPrefix(self, strs):
