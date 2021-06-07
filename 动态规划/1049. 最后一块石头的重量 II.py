e5c97d26e40eabd78fdec3b8ffed79369b3f30a7
MenNianShi <836705748@qq.com>
2021-06-08 00:44:42 +0800

    alter

---
+++ 动态规划/1049. 最后一块石头的重量 II.py
@@ -0,0 +1,29 @@
+#动态规划 同494。目标和
+# 定义二维布尔数组 dp，其中dp[i+1][j] 表示前 ii 个石头能否凑出重量 jj。特别地，dp[0][] 为不选任何石头的状态，因此除了
+# dp[0][0] 为真，其余 dp[0][j] 全为假。
+。
+class Solution(object):
+    def lastStoneWeightII(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        total = sum(stones)
+        n ,m= len(stones),total//2
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        dp[0][0]=True
+        for i in range(n):
+            for j in range(m + 1):
+                if j < stones[i]:
+                    dp[i + 1][j] = dp[i][j]
+                else:
+                    dp[i + 1][j] = dp[i][j] or dp[i][j - stones[i]]
+
+        ans = None
+        for j in range(m, -1, -1):
+            if dp[n][j]:
+                ans = total - 2 * j
+                break
+
+        return ans
+
