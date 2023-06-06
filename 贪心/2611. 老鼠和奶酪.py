3f568a5f1dffbb300a8602e6291a6c4f14aed835
MenNianShi <836705748@qq.com>
2023-06-07 00:44:24 +0800

    1

---
+++ 贪心/2611. 老鼠和奶酪.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def miceAndCheese(self, reward1, reward2, k):
+        ans = 0
+        n = len(reward1)
+        diffs = [reward1[i] - reward2[i] for i in range(n)]
+        ans += sum(reward2)
+        diffs.sort()
+        for i in range(1, k+1):
+            ans += diffs[n - i]
+        return ans
+
+
+
+
+
+
+
+
+
+
