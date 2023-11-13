0f99d7958fd609966fe69c65ce7c3a48e603e098
MenNianShi <836705748@qq.com>
2023-11-14 00:31:39 +0800

    1

---
+++ 715. Range 模块.py
@@ -0,0 +1,42 @@
+#线段树
+class RangeModule:
+
+    def __init__(self):
+        self.tree = defaultdict(int)
+
+    def pushdown(self, idx: int):
+        if self.tree[idx]:
+            self.tree[idx << 1] = self.tree[idx]
+            self.tree[idx << 1 | 1] = self.tree[idx]
+
+    def update(self, vl: int, start: int, end: int, l: int, r: int, idx: int):
+        if r < start or end < l:
+            return
+        if start <= l and r <= end:
+            self.tree[idx] = vl
+        else:
+            mid = (l + r) >> 1
+            self.pushdown(idx)
+            self.update(vl, start, end, l, mid, idx << 1)
+            self.update(vl, start, end, mid + 1, r, idx << 1 | 1)
+            self.tree[idx] = self.tree[idx << 1] & self.tree[idx << 1 | 1]
+
+    def query(self, start: int, end: int, l: int, r: int, idx: int):
+        if r < start or end < l:
+            return True
+        if start <= l and r <= end:
+            return self.tree[idx] == 1
+        else:
+            mid = (l + r) >> 1
+            self.pushdown(idx)
+            return self.query(start, end, l, mid, idx << 1) and self.query(start, end, mid + 1, r, idx << 1 | 1)
+
+    def addRange(self, left: int, right: int) -> None:
+        self.update(1, left, right - 1, 1, 10 ** 9, 1)
+
+    def queryRange(self, left: int, right: int) -> bool:
+        return self.query(left, right - 1, 1, 10 ** 9, 1)
+
+    def removeRange(self, left: int, right: int) -> None:
+        self.update(2, left, right - 1, 1, 10 ** 9, 1)
+
---
+++ 最短路径/1334. 阈值距离内邻居最少的城市.py
@@ -0,0 +1,65 @@
+
+# floyd
+class Solution(object):
+    def findTheCity(self, n, edges, distanceThreshold):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type distanceThreshold: int
+        :rtype: int
+        """
+        ans = [float('inf'), -1]
+        mp = [[float('inf')] * n for _ in range(n)]
+
+        for fr, to, weight in edges:
+            mp[fr][to], mp[to][fr] = weight, weight
+
+        for k in range(n):
+            mp[k][k] = 0
+            for i in range(n):
+                for j in range(n):
+                    mp[i][j] = min(mp[i][j], mp[i][k] + mp[k][j])
+
+        for i in range(n):
+            cnt = sum(mp[i][j] <= distanceThreshold for j in range(n))
+            if cnt <= ans[0]:
+                ans = [cnt, i]
+        return ans[1]
+#Dijkstra
+class Solution(object):
+    def findTheCity(self, n, edges, distanceThreshold):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type distanceThreshold: int
+        :rtype: int
+        """
+        ans = [float('inf'), -1]
+        mp = [[float('inf')] * n for _ in range(n)]
+        dis = [[float('inf')] * n for _ in range(n)]
+        visited = [[False] * n for _ in range(n)]
+
+        for fr, to, weight in edges:
+            mp[fr][to], mp[to][fr] = weight, weight
+
+        for k in range(n):
+            mp[k][k] = 0
+            for i in range(n):
+                for j in range(n):
+                    mp[i][j] = min(mp[i][j], mp[i][k] + mp[k][j])
+
+        for i in range(n):
+            dis[i][i] = 0
+            for j in range(n):
+                t = -1
+                for k in range(n):
+                    if not visited[i][k] and (t == -1 or dis[i][k] < dis[i][t]):
+                        t = k
+                for k in range(n):
+                    dis[i][k] = min(dis[i][k], dis[i][t] + mp[t][k])
+                visited[i][t] = True
+        for i in range(n):
+            cnt = sum(dis[i][j] <= distanceThreshold for j in range(n))
+            if cnt <= ans[0]:
+                ans = [cnt, i]
+        return ans[1]
