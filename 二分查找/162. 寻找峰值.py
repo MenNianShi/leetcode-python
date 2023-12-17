20cf8aaa3a85e54dd80c160aa4f0a60afc7ec48b
MenNianShi <836705748@qq.com>
2023-12-18 00:15:57 +0800

    update

--- 二分查找/162. 寻找峰值.py
+++ 二分查找/162. 寻找峰值.py
@@ -24,22 +24,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left =0
-        right = len(nums)-1
-        while left < right :
-            mid = left+ (right-left)//2
-            if nums[mid]>nums[mid+1]:
-                right = mid
-            else:
-                left = mid+1
-        return left
-
-class Solution(object):
-    def findPeakElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         n = len(nums)
 
         left = 0
@@ -60,3 +44,19 @@ class Solution(object):
             else:
                 right = mid -1
         return ans
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left =0
+        right = len(nums)-1
+        while left < right :
+            mid = left+ (right-left)//2
+            if nums[mid]>nums[mid+1]:
+                right = mid
+            else:
+                left = mid+1
+        return left
